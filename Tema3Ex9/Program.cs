6e07e5acf2ccc914d0169c386b30533374c99120
Gergoa123 <gergo3124@gmail.com>
2022-05-16 21:02:57 +0300

    Add project files.

---
+++ Tema3Ex9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tema3Ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inrtoduceti lungimea: ");
+            int lungime = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inrtoduceti latimea: ");
+            int latime = int.Parse(Console.ReadLine());
+
+            
+
+            double Calcdiagonala = Diagonal(latime, lungime);
+            
+
+            Console.WriteLine("Diagonala = " + Calcdiagonala);
+        }
+
+        static double Diagonal(int n, int m)
+        {
+            double diagonala = Math.Sqrt(n * n + m * m);
+
+            return diagonala;
+        }
+    }
+}
